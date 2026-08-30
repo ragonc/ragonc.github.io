08da07a314be1f3fe14fcc2f22077c826d40fc8f
Carmine Ragone <carmine.f.ragone@gmail.com>
2026-08-30 20:49:45 +0200

    cretion of drafts folder + removal of templates + start of homepage

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Run the Numbers';
+export const SITE_DESCRIPTION = 'A data analyst in Zurich running the numbers on training, budgets, and everything else.';
